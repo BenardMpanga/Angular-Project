3fbd3f5aa4de0e85715f4ebed7a51f6588df10ed
BenardMpanga <b30786@students.ucu.ac.ug>
2026-06-02 12:06:23 +0300

    Stabilize Cypress selectors and remove flakey wait
    Replace brittle DOM path selectors with attribute- and content-based selectors to make tests more robust (pagination uses .contains('3'), views use button[title], login uses [data-cy]). Remove an arbitrary cy.wait and add an explicit timeout when asserting the cm-map component. These changes improve test reliability and reduce flakiness.

--- cypress/e2e/customers.spec.cy.ts
+++ cypress/e2e/customers.spec.cy.ts
@@ -9,31 +9,30 @@ describe("Customers", () => {
     });
 
     it("should filter and display 10 customers", () => {
-        cy.wait(200); // pause to let cards load
         cy.get('[name="filter"]').type('ze');
         cy.get('.card').should('have.length', 1);
     });
 
     it("should navigate to page 3", () => {
-        cy.get('.pagination > :nth-child(4) > a').click();
+        cy.get('.pagination').contains('3').click();
         cy.get('.card').should('have.length', 2);
     });
 
     it("should display list view", () => {
         // Click List View
-        cy.get('.navbar > .nav > :nth-child(2) > a').click();
+        cy.get('button[title="List View"]').click();
         cy.get('tr').should('have.length.gt', 5);
     });
 
     it("should display map view", () => {
         // Click Map View
-        cy.get('.navbar > .nav > :nth-child(3) > a').click();
-        cy.get('cm-map').should('exist');
+        cy.get('button[title="Map View"]').click();
+        cy.get('cm-map', { timeout: 10000 }).should('exist');
     });
 
     it("should click New Customer and navigate to login", () => {
         // Click New Customer
-        cy.get('.navbar > .nav > :nth-child(4) > a').click();
+        cy.get('[data-cy="login-logout"]').click();
         cy.url().should('include', '/login');
     });
 
